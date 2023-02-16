402f0a1059c50a961769af4eb0bef8688a0ae6b9
Habtamu210 <beleyaha@gmail.com>
2023-02-16 00:49:52 -0800

    Update changes

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -69,7 +69,7 @@ const comments = () => {
       details.summary = content.summary;
     }
     details.name = content.name;
-    details.type = content.types;
+    details.type = content.type;
     details.release = content.premiered;
     details.image = content.image.original;
     showContent(id);
