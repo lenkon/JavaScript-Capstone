a4f3b4ff14b5e361a08ba96b386bc9301126370c
lenkon <inoulenkon@gmail.com>
2023-02-16 14:17:46 +0530

    Fix linters errors4

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// import './style/style.css';
-// import navbar from './modules/navbar.js';
-// import Media from './modules/media.js';
-// import comments from './modules/comments.js';
-
-// const media = new Media();
-// navbar();
-// media.showContents();
-// comments();
