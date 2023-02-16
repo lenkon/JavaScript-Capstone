72665b6524dad1d53a389d5efab56b3dbafd3262
lenkon <inoulenkon@gmail.com>
2023-02-16 14:15:58 +0530

    Fix linter errors3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import './style/style.css';
-import navbar from './modules/navbar.js';
-import Media from './modules/media.js';
-import comments from './modules/comments.js';
+// import './style/style.css';
+// import navbar from './modules/navbar.js';
+// import Media from './modules/media.js';
+// import comments from './modules/comments.js';
 
-const media = new Media();
-navbar();
-media.showContents();
-comments();
+// const media = new Media();
+// navbar();
+// media.showContents();
+// comments();
