f5c99cf148c71a1e61838a616c5fc3e6bc71020b
lenkon <inoulenkon@gmail.com>
2023-02-16 10:26:57 +0530

    Fix linters error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './style/style.css';
 import navbar from './modules/navbar.js';
 import Media from './modules/media.js';
 import comments from './modules/comments.js';
+import './style/style.css';
 
 const media = new Media();
 navbar();
